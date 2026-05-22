5c4055da6997a59c45d5201d60780dd607e75b4b
Bergamota <alves.freitas.tainara@escola.pr.gov.br>
2026-05-22 08:38:35 -0300

    Add files via upload

--- src/aula03.jsx
+++ src/aula03.jsx
@@ -1,8 +1,14 @@
 import React, {Component} from "react";
-import {View, Text, Image, StyleSheet, TextInput} from "react-native";
+import {View, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert} from "react-native";
 import { Button } from "react-native-web";
 
 class Aula03 extends Component {
+
+    handleLogin = () => {
+        Alert.alert("Ação", "Botão Entrar pressionando!")
+    }
+
+    
     render() {
         return(
             <View style={ styles.container}>
@@ -19,13 +25,12 @@ class Aula03 extends Component {
                     <Text style={styles.label}>Senha:</Text>
                     <TextInput 
                         style={ styles.input }
+                        secureTextEntry={true}
                     />
 
-                    <Button 
-                        style={ styles.botao }
-                        title="Entrar"
-                        color="green"
-                    />
+                    <TouchableOpacity style={styles.botao} onPress={this.handleLogin}>
+                        <Text style={styles.botaoTexto}>Entrar</Text>
+                    </TouchableOpacity>
 
                 </View>
             </View>
@@ -45,8 +50,6 @@ const styles = StyleSheet.create({
     },
 
     conteudo: {
-        borderColor: 'red',
-        borderWidth: 1,
         width: 300,
         height: 300,
         justifyContent: 'center',
@@ -76,7 +79,16 @@ const styles = StyleSheet.create({
         backgroundColor: 'green',
         padding: 20,
         color: 'black',
-        borderRadius: 5
+        borderRadius: 5,
+        marginTop: 15,
+        alignItems:'center',
+        width: 250
+    },
+
+    botaoTexto: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 
 })
